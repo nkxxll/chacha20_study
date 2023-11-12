610fb40da5a9e58d801cd8bf1dc4799e0dffc57b
nkirschall <niki.nice1203@gmail.com>
2023-11-12 18:32:57 +0100

    fix(chacha): fixed test script

--- run_test.sh
+++ run_test.sh
@@ -2,9 +2,7 @@
 
 echo "### STARTING TESTS ###"
 
-# compile
-gcc ./test_chacha.c ./chacha20.c -ov ./bin/test_chacha
-
+gcc test_chacha.c chacha20.c -o bin/test_chacha
 # execute
 ./bin/test_chacha
 
